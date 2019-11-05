eaa6da1cc4ae947c3a93e21f54eb0207ee29ec9b
NaveenKumar7845 <naveenbtechit2015@gmail.com>
2019-11-05 12:38:32 +0530

    5 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations"
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule,MatButtonModule} from '@angular/material';
 
 
 
@@ -20,7 +23,7 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations"
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, AppRoutingModuleModule,MatCardModule,MatFormFieldModule,MatInputModule,ReactiveFormsModule,MatSidenavModule,BrowserAnimationsModule ],
+  imports:      [ BrowserModule, FormsModule, AppRoutingModuleModule,MatCardModule,MatFormFieldModule,MatInputModule,ReactiveFormsModule,MatSidenavModule,MatToolbarModule,BrowserAnimationsModule,MatIconModule,MatListModule,MatButtonModule ],
   declarations: [ AppComponent, HelloComponent, MainComponentComponent, LoginComponent ],
   bootstrap:    [ AppComponent ],
 
--- src/app/main-component/main-component.component.ts
+++ src/app/main-component/main-component.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-component.component.css']
 })
 export class MainComponentComponent implements OnInit {
-showFiller = false;
+events: string[] = [];
+  opened: boolean;
   constructor() { 
 
   }
